174f6f4c0011db370ac35e5c2f94412c46b9d7b6
vivekdubeyvkd <vivekdubeyvkd@gmail.com>
2019-05-29 23:27:57 +0530

    Update configure-aws.sh

--- configure-aws.sh
+++ configure-aws.sh
@@ -1,5 +1,7 @@
 AWS_ACCESS_KEY_ID=""
 AWS_SECRET_ACCESS_KEY=""
+REGION="
 
 aws configure set aws_access_key_id ${AWS_ACCESS_KEY_ID}
 aws configure set aws_secret_access_key ${AWS_SECRET_ACCESS_KEY}
+aws configure set region ${REGION}
